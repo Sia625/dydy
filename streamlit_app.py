4463352b9913df55f454fe9e507cee503f2c4dd0
Sia625 <wusiyecuhk@163.com>
2025-05-19 19:09:51 +0800

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,18 @@
 import streamlit as st
 from page_content.home import home_page
-from page_content.experience import experience_page
-from page_content.contact import contact_page
 from page_content.resume import resume_page
+from page_content.contact import contact_page
 from page_content.education import education_page
+from page_content.experience import experience_page
 
 st.set_page_config(page_title="Wu Siye's Portfolio", layout="wide")
 
 pages = {
     "Home": home_page,
-    "Experience": experience_page,
     "Resume": resume_page,
+    "Contact": contact_page,
     "Education": education_page,
-    "Contact": contact_page
+    "Experience": experience_page
 }
 
 selected_page = st.sidebar.selectbox("Navigation", list(pages.keys()))
